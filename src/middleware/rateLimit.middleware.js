ba17f375d6ce46d9de68479bc529d927a0d44b2c
Bishal Nandi <nandibishal113@gmail.com>
2026-05-10 00:21:48 +0530

    fix(security): add custom rendering for rate limit responses

--- src/middleware/rateLimit.middleware.js
+++ src/middleware/rateLimit.middleware.js
@@ -3,50 +3,57 @@ const { RedisStore } = require("rate-limit-redis");
 const redis = require("../config/redis");
 const { ipKeyGenerator } = require("express-rate-limit");
 
+// SIGNUP RATE LIMITER
 const signupLimiter = rateLimit({
   store: new RedisStore({
-    sendCommand: (...args) =>
-      redis.call(...args),
+    sendCommand: (...args) => redis.call(...args),
   }),
-   keyGenerator: (req) =>
-    `signup:${ipKeyGenerator(req)}`,
+  keyGenerator: (req) => `signup:${ipKeyGenerator(req.ip)}`,
 
   windowMs: 15 * 60 * 1000, // 15 min
 
-  max: 5, 
-
-  message: {
-    error:
-      "Too many signup attempts. Please try again later.",
-  },
+  max: 5,
 
   standardHeaders: true,
 
   legacyHeaders: false,
+
+  handler: (req, res) => {
+    const old = { ...req.body };
+    delete old.password;
+
+    return res.status(429).render("signup", {
+      error: "Too many signup attempts. Please try again later.",
+      old,
+    });
+  },
 });
 
+// LOGIN RATE LIMITER
 const loginLimiter = rateLimit({
   store: new RedisStore({
-    sendCommand: (...args) =>
-      redis.call(...args),
+    sendCommand: (...args) => redis.call(...args),
   }),
-   keyGenerator: (req) =>
-    `login:${ipKeyGenerator(req)}`,
+  keyGenerator: (req) => `login:${ipKeyGenerator(req.ip)}`,
 
   windowMs: 15 * 60 * 1000, // 15 min
 
-  max: 10,
-
-  message: {
-    error:
-      "Too many login attempts. Please try again later.",
-  },
+  max: 20,
 
   standardHeaders: true,
 
   legacyHeaders: false,
-});
 
+  handler: (req, res) => {
+    const old = { ...req.body };
+    delete old.password;
+
+    return res.status(429).render("login", {
+      error: "Too many login attempts. Please try again later.",
+      old,
+    });
+  },
+});
 
 module.exports = {
   signupLimiter,
